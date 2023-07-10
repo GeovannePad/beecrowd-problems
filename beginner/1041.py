08d17f3ed0a4cfd7a5b51de94df8dacdbd176b99
Giovanni <geovannepadilha@hotmail.com>
2023-07-10 18:35:18 -0300

    Beginner 1041

---
+++ beginner/1041.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+x, y = map(float, input().split(" "))
+
+if x == 0 and y == 0:
+    print("Origem")
+elif x > 0 and y > 0:
+    print("Q1")
+elif x < 0 and y > 0:
+    print("Q2")
+elif x < 0 and y < 0:
+    print("Q3")
+elif x > 0 and y < 0:
+    print("Q4")
+elif x == 0 and y != 0:
+    print("Eixo Y")
+elif x != 0 and y == 0:
+    print("Eixo X")
