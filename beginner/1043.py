e7fd024a3cd628f0c554a00a0f264591320b5809
Giovanni <geovannepadilha@hotmail.com>
2023-07-10 22:15:47 -0300

    Beginner 1043

---
+++ beginner/1043.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+A, B, C = map(float, input().split(" "))
+
+if A > abs(B - C) and A < (B + C):
+    print(f"Perimetro = {A + B + C:.1f}")
+else:
+    print(f"Area = {(A + B) * C / 2:.1f}")
